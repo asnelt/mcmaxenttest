4e3133cc93c9d31d41f5c05a6729b7e83d4b837d
Arno Onken <asnelt@asnelt.org>
2017-01-18 00:09:06 +0100

    Restructured code to make it pylint clean.

--- demo.py
+++ demo.py
@@ -23,8 +23,16 @@ from mcmaxenttest import order2_poisson_test
 
 def main():
     '''
-    Main function for demonstration.
+    Main function for demonstration of maximum entropy test.
     '''
+    test_independent_poisson()
+    test_higher_order_poisson()
+
+def test_independent_poisson():
+    '''
+    Apply test to independent Poisson samples.
+    '''
+    print("Applying test to independent Poisson samples...")
     # Number of test repetitions
     n_trials = 20
     # Number of samples to draw in each trial
@@ -33,28 +41,39 @@ def main():
     alpha = 0.05
     # Poisson rate
     rate = 5
-    # Apply test to independent Poisson samples
-    print("Applying test to independent Poisson samples...")
     # Rejection results
     rejected_ind = np.zeros((n_trials, 1), dtype=bool)
     # p-values
-    p_value_ind = np.zeros((n_trials, 1))
+    p_values_ind = np.zeros((n_trials, 1))
     for i in range(n_trials):
         print(" Trial " + str(i+1) + " of " + str(n_trials))
         # Draw independent Poisson samples
         counts_0 = poisson.rvs([rate] * n_samples)
         counts_1 = poisson.rvs([rate] * n_samples)
         # Apply test
-        (rejected_ind[i], p_value_ind[i]) = order2_poisson_test(counts_0, \
+        (rejected_ind[i], p_values_ind[i]) = order2_poisson_test(counts_0, \
                 counts_1, alpha=alpha)
-    # Apply test to samples from a higher-order distribution
+    print_results("Independent Poisson samples:", rejected_ind, p_values_ind)
+
+def test_higher_order_poisson():
+    '''
+    Apply test to samples from a higher-order distribution.
+    '''
     print("Applying test to higher-order samples...")
+    # Number of test repetitions
+    n_trials = 20
+    # Number of samples to draw in each trial
+    n_samples = 100
+    # Significance level of the test
+    alpha = 0.05
+    # Poisson rate
+    rate = 5
     # Correlation parameters of mixture components
     rho = 0.9
     # Rejection results
     rejected_ho = np.zeros((n_trials, 1), dtype=bool)
     # p-values
-    p_value_ho = np.zeros((n_trials, 1))
+    p_values_ho = np.zeros((n_trials, 1))
     for i in range(n_trials):
         print(" Trial " + str(i+1) + " of " + str(n_trials))
         # Draw samples from a higher-order mixture distribution
@@ -68,15 +87,23 @@ def main():
         counts_0 = poisson.ppf(mix_0[:, 0], rate)
         counts_1 = poisson.ppf(mix_0[:, 1], rate)
         # Apply test
-        (rejected_ho[i], p_value_ho[i]) = order2_poisson_test(counts_0, \
+        (rejected_ho[i], p_values_ho[i]) = order2_poisson_test(counts_0, \
                 counts_1, alpha=alpha)
     # Print results
-    print("Independent Poisson samples:")
-    print(" Rejections:      " + str(rejected_ind.mean() * 100) + "%")
-    print(" Average p-value: " + str(p_value_ind.mean()))
-    print("Higher-order samples:")
-    print(" Rejections:      " + str(rejected_ho.mean() * 100) + "%")
-    print(" Average p-value: " + str(p_value_ho.mean()))
+    print_results("Higher-order samples:", rejected_ho, p_values_ho)
+
+def print_results(head_string, rejected, p_values):
+    '''
+    Prints the test results.
+
+    Args:
+        head_string: The heading to print.
+        rejected: Array of boolean test rejections.
+        p_values: Array of p-values.
+    '''
+    print(head_string)
+    print(" Rejections:      " + str(rejected.mean() * 100) + "%")
+    print(" Average p-value: " + str(p_values.mean()))
 
 if __name__ == "__main__":
     main()
--- mcmaxenttest.py
+++ mcmaxenttest.py
@@ -17,7 +17,7 @@ This module implements a second order maximum entropy test for count data.
 '''
 
 from scipy.optimize import root, minimize
-from scipy.stats import poisson
+from scipy.stats import entropy, poisson
 import numpy as np
 
 def order2_poisson_test(counts_0, counts_1, alpha=0.05, nmc=1000, maxiter=1000):
@@ -40,22 +40,20 @@ def order2_poisson_test(counts_0, counts_1, alpha=0.05, nmc=1000, maxiter=1000):
                   0 otherwise.
         p_value: p-value at the specified significance level.
     '''
-    # Generate contingency table
-    cont = np.zeros((int(counts_0.max())+1, int(counts_1.max())+1), \
-            dtype=np.float64)
-    for i in range(counts_0.size):
-        cont[int(counts_0[i]), int(counts_1[i])] += 1
-    emp_p = cont / counts_0.size
-    # Maximum entropy model for fixed marginals and correlation
-    (emp_lambda_0, emp_lambda_1, emp_correlation) = constraints(emp_p)
-    initial_point = [emp_lambda_0, emp_lambda_1, emp_correlation]
+    # Generate contingency table and empirical distribution
+    cont = contingency_table(counts_0, counts_1)
+    emp_pmf = cont / counts_0.size
+    # Empirical parameters of the distribution
+    emp_par = DistributionParameters.estimate_from(emp_pmf)
+    # Initialize survival function optimization
+    initial_point = emp_par.as_list()
     if maxiter < 1:
-        # Do not search for supremum of survival function
+        # Just use the initial point for the survival function
         max_survival = survival(initial_point, cont, nmc)
     else:
-        # Search for supremum of survival function
+        # Search for maximum of survival function
         fun = lambda theta: -survival(theta.T, cont, nmc)
-        # Constraints for probability mass functions
+        # Constraints for means and correlation
         bnds = [(np.finfo(float).eps, counts_0.max()+1.0), \
                 (np.finfo(float).eps, counts_1.max()+1.0), (-1.0, 1.0)]
         result = minimize(fun, initial_point, method='TNC', bounds=bnds, \
@@ -67,34 +65,38 @@ def order2_poisson_test(counts_0, counts_1, alpha=0.05, nmc=1000, maxiter=1000):
     rejected = p_value < alpha
     return (rejected, p_value)
 
+def contingency_table(counts_0, counts_1):
+    '''
+    Generate contingency table from counts
+    '''
+    cont = np.zeros((int(counts_0.max())+1, int(counts_1.max())+1), \
+            dtype=np.float64)
+    for i in range(counts_0.size):
+        cont[int(counts_0[i]), int(counts_1[i])] += 1
+    return cont
+
+
 def survival(theta, cont, nmc):
     '''
     Survival function of the test statistic.
     '''
-    # Extract arguments
-    total_sum = cont.sum()
-    lambda_0 = theta[0]
-    lambda_1 = theta[1]
-    correlation = theta[2]
+    pars = DistributionParameters(theta[0], theta[1], theta[2])
     marginal_0 = poisson.pmf(np.arange(np.ceil(np.maximum(cont.shape[0], \
-            poisson.isf(1e-4, lambda_0)+1))), lambda_0)
+            poisson.isf(1e-4, pars.mean_0)+1))), pars.mean_0)
     marginal_1 = poisson.pmf(np.arange(np.ceil(np.maximum(cont.shape[1], \
-            poisson.isf(1e-4, lambda_1)+1))), lambda_1)
+            poisson.isf(1e-4, pars.mean_1)+1))), pars.mean_1)
     # Find corresponding maximum entropy distribution
-    epmf = reference_pmf(marginal_0, marginal_1, correlation).flatten()
-    epmf = epmf[epmf > 0.0]
-    pmf = cont.flatten() / total_sum
-    pmf = pmf[pmf > 0.0]
-    # Divergence measure: Entropy difference
-    test_stat = abs((pmf * np.log2(pmf)).sum() - (epmf * np.log2(epmf)).sum())
+    pmf_ref = reference_pmf(marginal_0, marginal_1, pars.correlation).flatten()
+    pmf_emp = cont.flatten() / cont.sum()
+    # Calculate empirical test statistic
+    test_stat = divergence(pmf_emp, pmf_ref)
     # Find critical region via Monte Carlo sampling
     sample_stat = np.zeros((nmc,), dtype=np.float64)
     for i in range(nmc):
         # Draw a sample from the multinomial distribution
-        sample = np.random.multinomial(total_sum, epmf).flatten() / total_sum
-        sample = sample[sample > 0.0]
-        sample_stat[i] = abs((sample * np.log2(sample)).sum() \
-                - (epmf * np.log2(epmf)).sum())
+        pmf_sample = np.random.multinomial(cont.sum(), pmf_ref).flatten() \
+                / cont.sum()
+        sample_stat[i] = divergence(pmf_sample, pmf_ref)
     # Correct for ties
     n_ties = (test_stat == sample_stat).sum()
     uni = np.random.rand(n_ties+1)
@@ -102,6 +104,12 @@ def survival(theta, cont, nmc):
     value = ((test_stat < sample_stat).sum() \
             + (uni[-1] >= uni[1:n_ties]).sum()) / float(nmc)
     return value
+
+def divergence(pmf_0, pmf_1):
+    '''
+    Define distribution divergence as entropy difference.
+    '''
+    return abs(entropy(pmf_0) - entropy(pmf_1))
 
 def reference_pmf(marginal_0, marginal_1, correlation):
     '''
@@ -113,24 +121,24 @@ def reference_pmf(marginal_0, marginal_1, correlation):
     support_0 = np.arange(n_0)
     support_1 = np.arange(n_1)
     # Means of the marginals
-    lambda_0 = np.dot(marginal_0, support_0)
-    lambda_1 = np.dot(marginal_1, support_1)
+    mean_0 = np.dot(marginal_0, support_0)
+    mean_1 = np.dot(marginal_1, support_1)
     # Standard deviations of the marginals
-    sigma_0 = np.sqrt(np.dot(marginal_0, (support_0-lambda_0)**2))
-    sigma_1 = np.sqrt(np.dot(marginal_1, (support_1-lambda_1)**2))
-    product_moment = correlation*sigma_0*sigma_1+lambda_0*lambda_1
+    sigma_0 = np.sqrt(np.dot(marginal_0, (support_0-mean_0)**2))
+    sigma_1 = np.sqrt(np.dot(marginal_1, (support_1-mean_1)**2))
+    product_moment = correlation*sigma_0*sigma_1+mean_0*mean_1
     # Start with independent distribution
     initial_point = np.concatenate((marginal_0, marginal_1, [0.0]))
     # Find root
     result = root(maxent_errors, initial_point, args=(marginal_0, marginal_1, \
             product_moment))
     # Compute maximum entropy distribution
-    p_me = np.outer(result.x[0:n_0], result.x[n_0:(n_0+n_1)]) \
+    pmf_me = np.outer(result.x[0:n_0], result.x[n_0:(n_0+n_1)]) \
             * np.exp(result.x[-1] * np.outer(support_0, support_1))
     # Cut off negative values
-    p_me[p_me < 0] = 0
+    pmf_me[pmf_me < 0] = 0
     # Renormalize distribution
-    return p_me / p_me.sum()
+    return pmf_me / pmf_me.sum()
 
 def maxent_errors(point, marginal_0, marginal_1, product_moment):
     '''
@@ -156,26 +164,47 @@ def maxent_errors(point, marginal_0, marginal_1, product_moment):
     values[-1] = point_moment - product_moment
     return values
 
-def constraints(p):
+class DistributionParameters(object):
     '''
-    Constraints for the maximum entropy distribution.
+    This class represents the parameters of the maximum entropy distribution
+    with Poisson margins.
     '''
-    dim = p.shape
-    # Marginal distributions
-    marginal_0 = p.sum(axis=1)
-    marginal_1 = p.sum(axis=0)
-    support_0 = np.arange(dim[0])
-    support_1 = np.arange(dim[1])
-    # Expectations
-    lambda_0 = np.dot(marginal_0, support_0)
-    lambda_1 = np.dot(marginal_1, support_1)
-    # Standard deviations
-    sigma_0 = np.sqrt(np.dot(marginal_0, (support_0 - lambda_0)**2))
-    sigma_1 = np.sqrt(np.dot(marginal_1, (support_1 - lambda_1)**2))
-    # Correlation coefficient
-    if sigma_0 == 0 or sigma_1 == 0:
-        correlation = 0.0
-    else:
-        correlation = (np.multiply(p, np.outer(support_0, support_1)).sum() \
-                - lambda_0*lambda_1) / (sigma_0*sigma_1)
-    return (lambda_0, lambda_1, correlation)
+    def __init__(self, mean_0, mean_1, correlation):
+        '''
+        Constructor setting means and correlation.
+        '''
+        self.mean_0 = mean_0
+        self.mean_1 = mean_1
+        self.correlation = correlation
+
+    def as_list(self):
+        '''
+        Returns all parameters as a list.
+        '''
+        return [self.mean_0, self.mean_1, self.correlation]
+
+    @staticmethod
+    def estimate_from(pmf):
+        '''
+        Estimates parameters from given probability mass function.
+        '''
+        size = pmf.shape
+        # Marginal distributions
+        marginal_0 = pmf.sum(axis=1)
+        marginal_1 = pmf.sum(axis=0)
+        support_0 = np.arange(size[0])
+        support_1 = np.arange(size[1])
+        # Expectations
+        mean_0 = np.dot(marginal_0, support_0)
+        mean_1 = np.dot(marginal_1, support_1)
+        # Standard deviations
+        sigma_0 = np.sqrt(np.dot(marginal_0, (support_0 - mean_0)**2))
+        sigma_1 = np.sqrt(np.dot(marginal_1, (support_1 - mean_1)**2))
+        # Correlation coefficient
+        if sigma_0 == 0 or sigma_1 == 0:
+            correlation = 0.0
+        else:
+            correlation = (np.multiply(pmf, \
+                    np.outer(support_0, support_1)).sum() - mean_0*mean_1) \
+                    / (sigma_0*sigma_1)
+        return DistributionParameters(mean_0, mean_1, correlation)
