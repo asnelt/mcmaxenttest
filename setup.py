3b52ff99028970b7223b83fb5ae4c29355bee438
Arno Onken <asnelt@asnelt.org>
2017-01-31 12:02:45 +0100

    Corrected script extension.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     author_email="asnelt@asnelt.org",
     license="GPL-3.0",
     packages=["mcmaxenttest"],
-    scripts=['demo_mcmaxenttest'],
+    scripts=['demo_mcmaxenttest.py'],
     install_requires=["scipy"],
     test_suite="nose2.collector.collector"
 )
